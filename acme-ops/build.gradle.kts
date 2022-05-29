67b211210c27e4721e0c23676371af7990267d8e
Matt Wright <matt@nobien.net>
2022-05-29 13:13:15 -0400

    Fix kube ports

--- acme-ops/build.gradle.kts
+++ acme-ops/build.gradle.kts
@@ -14,64 +14,23 @@ tasks {
     workingDir(sslCertDir)
   }
 
-  val devKubernetesClusterStart = register<Exec>("devMinikubeStart") {
+  val devKubernetesClusterStart = register<Exec>("devKubernetesClusterStart") {
     commandLine(
       "bash", "-c", """
-        k3d cluster create $devKubernetesClusterName -p "8888:80@loadbalancer" -p "4433:443@loadbalancer" --k3s-arg "--disable=traefik@server:0"
+        k3d cluster create $devKubernetesClusterName -p "80:80@loadbalancer" -p "443:443@loadbalancer" --k3s-arg "--disable=traefik@server:0"
         kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.2.0/deploy/static/provider/cloud/deploy.yaml
       """.trimIndent()
     )
     dependsOn(makeCerts)
   }
 
-  // val devMinikubeAddons = register<Exec>("devMinikubeAddons") {
-  //   commandLine("minikube", "--profile", devKubernetesClusterName, "addons", "enable", "ingress")
-  //   dependsOn(devKubernetesClusterStart)
-  // }
-
-  // val ingressNginxUrl = register<Exec>("ingressNginxUrl") {
-  //   commandLine("bash", "-c", """
-  //     mkdir -p $(dirname $ingressNginxUrlFile)
-  //     minikube --profile $devKubernetesClusterName service --namespace ingress-nginx ingress-nginx-controller --url | \
-  //       tail -n 1 | \
-  //       sed -e 's/http:\/\//https:\/\//' > $ingressNginxUrlFile
-  //     echo "Ingress Nginx URL written to $ingressNginxUrlFile"
-  //   """.trimIndent())
-  //   dependsOn(devMinikubeAddons)
-  // }
-
-  // val devExpandNginxConfig = register<Copy>("devExpandNginxConfig") {
-  //   from("src/nginx/dev")
-  //   into(devNginxConfFile.parent)
-  //   include("acme.conf.template")
-  //   rename { it.replace(".template", "") }
-  //   expand(
-  //     "sslCertificate" to "$sslCertificateFile",
-  //     "sslCertificateKey" to "$sslCertificateKeyFile",
-  //     "proxyPass" to ingressNginxUrlFile
-  //   )
-  //   dependsOn(ingressNginxUrl)
-  //   doLast {
-  //     exec {
-  //       commandLine("echo", """
-  //         Nginx configuration may have changed. Reload the service:
-  //         [Ubuntu] $ sudo systemctl reload nginx.service
-  //         [macOS]  $ sudo brew services restart nginx
-  //       """.trimIndent())
-  //     }
-  //   }
-  // }
-
   register("devStart") {
     dependsOn(
       devKubernetesClusterStart,
-      // devMinikubeAddons,
-      // devExpandNginxConfig,
     )
   }
 
   register<Exec>("devDelete") {
     commandLine("k3d", "cluster", "delete", devKubernetesClusterName)
-    // commandLine("minikube", "--profile", devKubernetesClusterName, "delete")
   }
 }
