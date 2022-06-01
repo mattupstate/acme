a677ab907bc67f3cd92e52b767fea517c57390ad
Matt Wright <matt@nobien.net>
2022-05-31 21:02:56 -0400

    Upgrade spotless

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,7 +13,7 @@ dependencies {
     implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:1.6.21")
     implementation("org.jetbrains.kotlin.plugin.serialization:org.jetbrains.kotlin.plugin.serialization.gradle.plugin:1.6.21")
     implementation("org.jlleitschuh.gradle:ktlint-gradle:10.2.1")
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.5.1")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.6.1")
   }
 }
 
