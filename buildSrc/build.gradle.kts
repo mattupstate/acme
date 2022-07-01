e721bd07b414979fbf4be2ac1fa957733c917ac7
Matt Wright <matt@nobien.net>
2022-07-01 11:01:24 -0400

    polish buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,10 +17,7 @@ dependencies {
   }
 }
 
-tasks {
-  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    kotlinOptions {
-      jvmTarget = "11"
-    }
-  }
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
 }
--- buildSrc/src/main/kotlin/acme.kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/acme.kotlin-conventions.gradle.kts
@@ -29,6 +29,7 @@ version = "0.1.0"
 
 java {
   sourceCompatibility = JavaVersion.VERSION_11
+  targetCompatibility = JavaVersion.VERSION_11
 }
 
 jacoco {
@@ -37,7 +38,6 @@ jacoco {
 
 spotless {
   kotlin {
-    // version and userData are both optional
     ktlint("0.44.0")
       .userData(mapOf("indent_size" to "2", "continuation_indent_size" to "2"))
   }
@@ -62,7 +62,7 @@ tasks {
 
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
-      jvmTarget = "11"
+      jvmTarget = JavaVersion.VERSION_11.toString()
       freeCompilerArgs = listOf(
         "-Xemit-jvm-type-annotations",  // NOTE: necessary for validation annotations to work. Such as: `val things: List<@NotBlank String>`
         "-opt-in=kotlin.RequiresOptIn",
