c4318e88eeacee747e16b14d2352cd683e056ff6
Matt Wright <mdw1980@gmail.com>
2022-11-19 22:10:08 -0500

    Restor reflect dep

--- acme-lib/acme-lib-selenium/build.gradle.kts
+++ acme-lib/acme-lib-selenium/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 }
 
 dependencies {
+  implementation(kotlin("reflect"))
   api(libs.org.seleniumhq.selenium.selenium.java)
   api(libs.io.kotest.kotest.assertions.core)
   testImplementation(libs.io.kotest.kotest.runner.junit5)
