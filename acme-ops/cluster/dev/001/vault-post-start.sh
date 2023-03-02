08a27348795688d5b9999d2fa64b1b6980d87038
Matt Wright <mdw1980@gmail.com>
2023-03-01 21:19:40 -0500

    Fix vault-post-start.sh

--- acme-ops/cluster/dev/001/vault-post-start.sh
+++ acme-ops/cluster/dev/001/vault-post-start.sh
@@ -5,6 +5,12 @@ sleep 10
 wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -O /home/vault/jq
 chmod +x /home/vault/jq
 
-vault operator init -n 1 -t 1 -format=json > /home/vault/init.json
+vault status -format=json > /home/vault/status.json
 
-vault operator unseal "$(/home/vault/jq -r '.unseal_keys_b64[0]' /home/vault/init.json)"
+if /home/vault/jq -e '.initialized == false' /home/vault/status.json; then
+    vault operator init -n 1 -t 1 -format=json > /vault/data/init.json
+fi
+
+if /home/vault/jq -e '.sealed == true' /home/vault/status.json; then
+    vault operator unseal "$(/home/vault/jq -r '.unseal_keys_b64[0]' /vault/data/init.json)"
+fi
